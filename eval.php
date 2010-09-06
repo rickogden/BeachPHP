002cb9b3131118d492198c5addd6a8504bced1f9
Ryan Mauger <ryan@lupimedia.com>
2010-09-06 14:57:55 +0100

    Added more in-depth error handling

--- eval.php
+++ eval.php
@@ -1,4 +1,5 @@
 <?php
+include 'images.php';
 $tmpdir = ini_get('upload_tmp_dir');
 $file = tempnam($tmpdir,'php');
 $data = $_POST['data'];
@@ -6,8 +7,36 @@ if (!stristr($data, '<?php')) {
 	$data = "<?php\n" . $data;
 }
 file_put_contents($file, $data);
-system  ('php -l ' . $file);
-echo '<hr/><br/>';
+$output = array();
+$return = null;
+exec  ('php -d error_reporting=32767 -d display_errors=1 -d log_errors=0 -l ' . $file, $output, $return);
+$message = 'OK!';
+$image = 'ok';
+$errLine;$match;
+if (0 !== $return) {
+	$matches = array();
+	$error = $output[1];
+	$match = preg_match('#([\w\d\s]+), (.*) (?:in [\w\d/]+ on line) ([\d]+)#i', $error, $matches);
+	if ($match) {
+		$error = "[{$matches[1]}:{$matches[2]}][LINE:{$matches[3]}]";
+		$errLine = $matches[3];
+	}
+	$message = 'ERROR PARSING INPUT: ' . $error;
+	$image = 'warning';
+}
+
+echo "<img src=\"data:image/jpeg;base64,{$$image}\"/>&nbsp;{$message}<hr/><br/>";
+if (0 != $return && $match) {
+	echo '<div style="width:100%;height:100%;overflow:auto;white-space:nowrap;border-bottom:1px dotted #2b2b2b;">';
+	$data = explode("\n",$data);
+	foreach ($data as $line => &$value) {
+        $color = ($line == $errLine-1) ? 'red' : 'lime';
+    	$value = '<div style="background:'.$color.';margin:0px;">' . $line .': &nbsp;' . htmlentities($value) . "</div>\n";
+echo $value;
+    }
+echo '</div>';
+	return;
+}
 error_reporting(E_ALL|E_STRICT);
 ini_set('display_errors', 'on');
 ini_set('open_basedir', implode(PATH_SEPARATOR, array(__DIR__,dirname($file))) . PATH_SEPARATOR . get_include_path());
